191ef3ba45ec8b5c34a3057de7af1e0b328ee629
asd668957 <asd668957@gmail.com>
2018-04-13 11:01:32 +0800

    zongan

---
+++ MUS1062-Homework-SQL/Career.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MUS1062_Homework_SQL
+{
+    class Career
+    {
+        public string 資料集名稱 { get; set; }
+        public string 服務分類 { get; set; }
+    }
+}
