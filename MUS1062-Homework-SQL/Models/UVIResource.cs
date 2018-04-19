c05e06e2b8642adb580ad53764f9ee93bc0159f3
Chih-Chieh Lin <37201153+Jacklintw@users.noreply.github.com>
2018-04-19 19:08:48 +0800

    Add files via upload

---
+++ MUS1062-Homework-SQL/Models/UVIResource.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MUS1062_Homework_SQL
+{
+    class UVIResource
+    {
+        public string 城市 { set; get; }
+        public string 發布機關 { set; get; }
+        public string 發布地區 { set; get; }
+        public string 紫外線指數 { set; get; }
+        public string 發布時間 { set; get; }
+    }
+}
